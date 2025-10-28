4984f095e3e067c7b0604637c940c1b9e54ae4ed
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-10-28 13:26:49 +0900

    Added navbar component with basic styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,18 @@
 import styles from "../styles/navbar.module.css"
+import icon from "../assets/icon.svg"
 
 function NavBar(){
   return (
     <div className={styles.navbar}>
-      <a className={styles.link}>Home</a>
-      <a className={styles.link}>Shop</a>
-      <a className={styles.link}>Cart</a>
-      <p>1</p>
+      <div className={styles.imgContainer}>
+        <img src={icon} className={styles.img}></img>
+      </div>
+
+      <div className={styles.linkContainer}>
+        <a className={styles.link}>Shop</a>
+        <a className={styles.link}>Cart</a>
+        <p className={styles.button}>1</p>
+      </div>
     </div>
   );
 }
