094703ec20f84be7838034a058597f1ebfdd8a20
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-10-28 13:49:51 +0900

    Added functional link navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import './App.css'
 
 import NavBar from './components/navbar'
+import HomePage from './components/homepage'
+import ShopPage from './components/shoppage';
+import CartPage from './components/cartpage';
+
+import { useState } from 'react';
 
 function App() {
+  const [page, setPage] = useState(0);
+
+  const handlePage = (num, event) => {
+    event.preventDefault()
+    setPage(num)
+  }
+
   return (
-    <NavBar />
-  )
+    <>
+      <NavBar handlePage={handlePage} />
+
+      {page === 0 && (
+        <HomePage />
+      )}
+      {page === 1 && (
+        <ShopPage />
+      )}
+      {page === 2 && (
+        <CartPage />
+      )}
+      
+    </>
+  );
 }
 
 export default App
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,16 @@
 import styles from "../styles/navbar.module.css"
 import icon from "../assets/icon.svg"
 
-function NavBar(){
+function NavBar(props){
   return (
     <div className={styles.navbar}>
       <div className={styles.imgContainer}>
-        <img src={icon} className={styles.img}></img>
+        <img src={icon} className={styles.img} onClick={(event) => props.handlePage(0, event)}></img>
       </div>
 
       <div className={styles.linkContainer}>
-        <a className={styles.link}>Shop</a>
-        <a className={styles.link}>Cart</a>
+        <a className={styles.link} onClick={(event) => props.handlePage(1, event)}>Shop</a>
+        <a className={styles.link} onClick={(event) => props.handlePage(2, event)}>Cart</a>
         <p className={styles.button}>1</p>
       </div>
     </div>
