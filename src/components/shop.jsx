c48a268d7a8c7863500d0430f07f84b409bd7d54
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-10-29 12:25:33 +0900

    Styling of shop page

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -11,7 +11,8 @@ function Shop(props){
           {props.data.map(item => (
             <div className={styles.gridItem} key={item.id}>
               <p className={styles.itemName}>{item.title}</p>
-              <p>${item.price}</p>
+              <p className={styles.category}>Category: {item.category}</p>
+              <p className={styles.price}>${item.price}</p>
               <img src={item.image} className={styles.image}></img>
             </div>
           ))}
