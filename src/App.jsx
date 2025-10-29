d5d641693fdda757180b5beced81e545835eb9b3
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-10-29 10:39:54 +0900

    Fetched data from FakeStore API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css'
+import React, { useEffect } from 'react';
 
 import NavBar from './components/navbar';
 import Home from './components/home';
 
+const URL = `https://fakestoreapi.com/products`
+
 function App() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(URL);
+      const data = await response.json()
+      console.log(data)
+    }
+
+    fetchData(); 
+  }, [])
+
+
   return (
     <>
       <NavBar />
